49d9a046f00e14ea119dcd0baad84f34be38b1d5
Mark Nicol <marc.nuc@gmail.com>
2025-08-31 15:56:58 -0400

    Add Astro GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://mnicol1.github.io/capucine-et-tournesol',
   base: '/capucine-et-tournesol/',
 });
